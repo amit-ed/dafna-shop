6a19eb14989b4dd85f0eeae83fe8450f527b1c74
eindor <eindor13@gmail.com>
2022-11-18 17:10:23 +0200

    product list fully responsive

--- src/app/products.ts
+++ src/app/products.ts
@@ -27,5 +27,12 @@ export let products = [
     name : "ינשוף",
     description: "מגניב אפשר לשים אותו בסלון",
     price: 299,
+  },
+  {
+    id: 4,
+    src: "https://cdn.shopify.com/s/files/1/0608/7907/7609/products/120342-Imana-Necklace-1_360x.jpg?v=1643745531",
+    name : "שולחן",
+    description: "נעשה בעבודת יד נקודות",
+    price: 155,
   }
 ];
